751bf1484c9f6edb24c501bb569d633d9988556c
Nehra <deepaknehra416@gmail.com>
2024-07-05 19:35:37 +0530

    Update

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import Lottie from 'lottie-react'
 import A1 from "./assets/A1.json"
-import A2 from "./assets/A2.json"
 import A3 from "./assets/A3.json"
-import A4 from "./assets/A4.json"
 import A5 from "./assets/A5.json"
-import A6 from "./assets/A6.json"
 import A7 from "./assets/A7.json"
-import { Link } from 'react-router-dom'
-
+import "./Card.css"
 
 export const Card = (props) => {
     const total = props.quates.length;
@@ -16,6 +12,7 @@ export const Card = (props) => {
     const [value, setvalue] = useState("Nehra")
     const [input, setinput] = useState()
     const [quate, setquate] = useState()
+    const [pop, setpop] = useState(false)
 
     useEffect(() => {
         const index = Math.floor(Math.random() * total);
@@ -27,72 +24,70 @@ export const Card = (props) => {
         setinput(event.target.value)
     }
     const Generate = () => {
-        setvalue(input);
+        setvalue(input || "Nehra");
+        popup();
     }
 
     const change_quate = () => {
         const index = Math.floor(Math.random() * total);
         setquate(props.quates[index]);
-        console.log("hii");
+    }
+
+    const popup = () => {
+        setpop(!pop)
+        console.log(pop)
+        console.log("working")
     }
     return (
-        <div className='bg-black h-[160vh] text-white'>
+        <div className={` bg-black h-full text-white `}>
 
-            <Lottie animationData={A1} className='w-[500px] h-[480px]' />
-            <Lottie animationData={A3} className='w-[500px] h-[350px] absolute top-0 right-0' />
-            <Lottie animationData={A5} className='w-[200px] h-[150px] absolute top-[450px] left-0' />
-            <Lottie animationData={A7} className='w-[240px] h-[180px] absolute top-[300px] left-[570px]' />
+            {/* Animations */}
+            <Lottie animationData={A1} className='w-[500px] h-[480px] ' />
+            <Lottie animationData={A3} className='w-[500px] h-[350px] absolute top-0 right-0 ' />
+            <Lottie animationData={A5} className='w-[200px] h-[150px] absolute top-[450px] left-0 ' />
+            <Lottie animationData={A7} className='w-[240px] h-[180px] absolute top-[300px] left-[570px] ' />
 
+            {/* Main content */}
             <div className='absolute top-10 right-10'>
-                <p className='text-4xl text-[#ffbf00] '>In Advance</p>
-                <h1 className='text-7xl'>Happy Diwali</h1>
-                <p className='text-3xl pt-5 text-left indent-28 text-gray-300 animate-pulse'>to you and your family!</p>
+                <p className='text-4xl text-[#ffbf00] ' style={{ fontFamily: "'Playwrite GB S', cursive" }}>In Advance</p>
+                <h1 className='text-7xl' style={{ fontFamily: "'Rubik', sans-serif" }} >Happy Diwali</h1>
+                <p className='text-3xl pt-5 text-left indent-28 text-gray-300 animate-pulse' style={{ fontFamily: "'Playwrite GB S', cursive" }}>to you and your family!</p>
                 <div className='text-4xl mt-10'>
-                    <p className='text-right'>REGARDS,</p>
-                    <p className='text-right text-sky-800 font-bold hover:underline '>{value}</p>
+                    <p className='text-right' style={{ fontFamily: "'Rubik', sans-serif" }}>REGARDS,</p>
+                    <p className='text-right text-sky-800 font-bold hover:underline ' style={{ fontFamily: "'Abel', sans-serif" }}>{value}</p>
                 </div>
             </div>
-            <div className='text-[#ffbf00] text-4xl px-44 text-center m-auto cursor-pointer h-24 ' onClick={change_quate}>
+
+            {/* Quates */}
+            <div className='text-[#ffbf00] text-4xl px-44 text-center m-auto cursor-pointer h-24 ' onClick={change_quate} style={{ fontFamily: "'Rubik', sans-serif" }}>
                 {quate}
             </div>
+
+            {/* For new Name Card */}
             <div className='text-center mt-10 bg-gray-900 p-5'>
-                <p className='text-2xl pb-5'>Create your own Card with your Name</p>
-                <input type="text" placeholder='Nehra' onChange={Get} className='py-2 px-3 text-xl font-semibold border-2 rounded text-gray-600' />
-                <input type="button" value="Generate" onClick={Generate} className='bg-gray-500 py-2 px-3 rounded text-xl ml-3 cursor-pointer' />
+                <p className='text-2xl pb-5' style={{ fontFamily: "'Abel', sans-serif" }}>Create your own Card with your Name</p>
+                <input type="button" value="Click Here" onClick={popup} className='bg-gray-500 py-2 px-3 rounded text-xl ml-3 cursor-pointer' />
             </div>
+
+            {/* For Popup */}
+            <div className={`popup ${pop ? "openPopup" : ""} bg-gray-200`} style={{ boxShadow: '0px 0px 30px white' }}>
+                <h1 className='text-black text-2xl pb-5 text-center font-semibold'>Enter Your Name :)</h1>
+                <input type="text" placeholder='Nehra' onChange={Get} className='py-2 px-3 text-xl font-semibold border-2 rounded text-gray-600 mb-5 shadow' />
+                <input type="button" value="Create Now" onClick={Generate} className='bg-sky-800 py-1 w-full rounded text-xl cursor-pointer text-center shadow-2xl' />
+            </div>
+
+            {/* Share partition */}
             <div className='text-center mt-10 bg-gray-900 p-5'>
-                <p>Let's Share on</p>
+                <p className='text-xl' style={{ fontFamily: "'Abel', sans-serif" }}>Let's Share on</p>
                 <div className='flex justify-center gap-5 mt-5'>
-                    <a href="https://www.facebook.com/">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-                            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
-                        </svg>
-                    </a>
-                    <a href="https://web.whatsapp.com/">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
-                            <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
-                        </svg>
-                    </a>
-                    <a href="https://x.com/?lang=en">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
-                            <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
-                        </svg>
-                    </a>
-                    <a href="https://in.linkedin.com/">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-                            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
-                        </svg>
-                    </a>
-                    <a href="https://web.snapchat.com/">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-snapchat" viewBox="0 0 16 16">
-                            <path d="M15.943 11.526c-.111-.303-.323-.465-.564-.599a1 1 0 0 0-.123-.064l-.219-.111c-.752-.399-1.339-.902-1.746-1.498a3.4 3.4 0 0 1-.3-.531c-.034-.1-.032-.156-.008-.207a.3.3 0 0 1 .097-.1c.129-.086.262-.173.352-.231.162-.104.289-.187.371-.245.309-.216.525-.446.66-.702a1.4 1.4 0 0 0 .069-1.16c-.205-.538-.713-.872-1.329-.872a1.8 1.8 0 0 0-.487.065c.006-.368-.002-.757-.035-1.139-.116-1.344-.587-2.048-1.077-2.61a4.3 4.3 0 0 0-1.095-.881C9.764.216 8.92 0 7.999 0s-1.76.216-2.505.641c-.412.232-.782.53-1.097.883-.49.562-.96 1.267-1.077 2.61-.033.382-.04.772-.036 1.138a1.8 1.8 0 0 0-.487-.065c-.615 0-1.124.335-1.328.873a1.4 1.4 0 0 0 .067 1.161c.136.256.352.486.66.701.082.058.21.14.371.246l.339.221a.4.4 0 0 1 .109.11c.026.053.027.11-.012.217a3.4 3.4 0 0 1-.295.52c-.398.583-.968 1.077-1.696 1.472-.385.204-.786.34-.955.8-.128.348-.044.743.28 1.075q.18.189.409.31a4.4 4.4 0 0 0 1 .4.7.7 0 0 1 .202.09c.118.104.102.26.259.488q.12.178.296.3c.33.229.701.243 1.095.258.355.014.758.03 1.217.18.19.064.389.186.618.328.55.338 1.305.802 2.566.802 1.262 0 2.02-.466 2.576-.806.227-.14.424-.26.609-.321.46-.152.863-.168 1.218-.181.393-.015.764-.03 1.095-.258a1.14 1.14 0 0 0 .336-.368c.114-.192.11-.327.217-.42a.6.6 0 0 1 .19-.087 4.5 4.5 0 0 0 1.014-.404c.16-.087.306-.2.429-.336l.004-.005c.304-.325.38-.709.256-1.047m-1.121.602c-.684.378-1.139.337-1.493.565-.3.193-.122.61-.34.76-.269.186-1.061-.012-2.085.326-.845.279-1.384 1.082-2.903 1.082s-2.045-.801-2.904-1.084c-1.022-.338-1.816-.14-2.084-.325-.218-.15-.041-.568-.341-.761-.354-.228-.809-.187-1.492-.563-.436-.24-.189-.39-.044-.46 2.478-1.199 2.873-3.05 2.89-3.188.022-.166.045-.297-.138-.466-.177-.164-.962-.65-1.18-.802-.36-.252-.52-.503-.402-.812.082-.214.281-.295.49-.295a1 1 0 0 1 .197.022c.396.086.78.285 1.002.338q.04.01.082.011c.118 0 .16-.06.152-.195-.026-.433-.087-1.277-.019-2.066.094-1.084.444-1.622.859-2.097.2-.229 1.137-1.22 2.93-1.22 1.792 0 2.732.987 2.931 1.215.416.475.766 1.013.859 2.098.068.788.009 1.632-.019 2.065-.01.142.034.195.152.195a.4.4 0 0 0 .082-.01c.222-.054.607-.253 1.002-.338a1 1 0 0 1 .197-.023c.21 0 .409.082.49.295.117.309-.04.56-.401.812-.218.152-1.003.638-1.18.802-.184.169-.16.3-.139.466.018.14.413 1.991 2.89 3.189.147.073.394.222-.041.464" />
-                        </svg>
-                    </a>
+                    {props.svg.map((item) => {
+                        return (
+                            <a href="#">{item}</a>
+                        )
+                    })}
+
                 </div>
             </div>
         </div>
     )
 }
-// {props.quates[index]} <br />
-// {index} <br />
-// {props.quates[index]}
